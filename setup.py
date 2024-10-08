10fd68f771b250850350ee2f739e56c7ee539a7f
Chidozie Nwoga <chidozie@Chidozies-MacBook-Pro-2.fritz.box>
2024-10-08 21:49:33 +0200

    setup: setting up the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='video_quality_assessment',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Video Quality Assessment using Machine Learning',
+    author='Your Name',
+    license='MIT',
+)
